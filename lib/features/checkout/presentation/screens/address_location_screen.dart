3feaa22e81376bbf3fb302b2341a3108ccb2445e
Omar Assem <omarassem996@gmail.com>
2023-01-11 16:57:43 +0200

    google maps
    - add access location permissions
    - add google maps api key
    - address location screen enhancements

--- lib/features/checkout/presentation/screens/address_location_screen.dart
+++ lib/features/checkout/presentation/screens/address_location_screen.dart
@@ -1,4 +1,5 @@
 import 'package:bakery/core/presentation/resources/routes_manager.dart';
+import 'package:bakery/core/presentation/resources/values_manager.dart';
 import 'package:bakery/core/presentation/util/toast.dart';
 import 'package:bakery/core/presentation/widgets/custom_elevated_button.dart';
 import 'package:bakery/generated/l10n.dart';
@@ -15,22 +16,17 @@ class AddressLocationScreen extends StatefulWidget {
 }
 
 class _AddressLocationScreenState extends State<AddressLocationScreen> {
-  String address = 'search';
-  double mapPadding = 0;
-  LatLng? _currentPosition;
   Marker _origin = const Marker(markerId: MarkerId("value"));
-  Geolocator geolocator = Geolocator();
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(31.205753, 29.924526),
-    zoom: 14.4746,
-  );
+  LatLng? _currentPosition;
   late GoogleMapController _controller;
   late double _subtotal;
 
   @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    _subtotal = ModalRoute.of(context)!.settings.arguments! as double;
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) => _subtotal = ModalRoute.of(context)!.settings.arguments! as double,
+    );
   }
 
   @override
@@ -39,19 +35,19 @@ class _AddressLocationScreenState extends State<AddressLocationScreen> {
       body: Stack(
         children: [
           GoogleMap(
-            initialCameraPosition: _kGooglePlex,
-            padding: EdgeInsets.only(bottom: mapPadding),
-            onMapCreated: (GoogleMapController mapController) {
+            initialCameraPosition: const CameraPosition(
+              target: LatLng(31.205753, 29.924526),
+              zoom: 14.4746,
+            ),
+            onMapCreated: (mapController) {
               _controller = mapController;
-              locatePosition();
-              setState(() {
-                mapPadding = 265.0;
-              });
+              _locatePosition();
+              setState(() {});
             },
             myLocationEnabled: true,
-            onTap: (position) {
-              _currentPosition = position;
-              addMarker(position);
+            onTap: (newPosition) {
+              _currentPosition = newPosition;
+              _addMarker(newPosition);
             },
             markers: {
               _origin,
@@ -61,7 +57,7 @@ class _AddressLocationScreenState extends State<AddressLocationScreen> {
             alignment: Alignment.bottomRight,
             child: FloatingActionButton(
               onPressed: () async {
-                addMarker(await locatePosition());
+                _addMarker(await _locatePosition());
               },
               child: const Icon(Icons.share_location_sharp),
             ),
@@ -69,17 +65,17 @@ class _AddressLocationScreenState extends State<AddressLocationScreen> {
           Align(
             alignment: Alignment.bottomCenter,
             child: Container(
-              margin: const EdgeInsets.only(bottom: 25),
+              margin: const EdgeInsets.only(bottom: Insets.xl),
               child: SizedBox(
-                height: 45,
-                width: 231,
+                height: Sizes.s40,
+                width: MediaQuery.of(context).size.width * 0.52,
                 child: CustomElevatedButton(
                   onPressed: () async {
                     Navigator.of(context).pushNamed(
                       AppRoutes.checkout,
                       arguments: CheckoutArguments(
-                        address: 'Alex',
-                        //await getAddressFromLatLong(_currentPosition!),
+                        address:
+                            await _getAddressFromLatLong(_currentPosition!),
                         subtotal: _subtotal,
                       ),
                     );
@@ -100,7 +96,7 @@ class _AddressLocationScreenState extends State<AddressLocationScreen> {
     super.dispose();
   }
 
-  void addMarker(LatLng? pos) {
+  void _addMarker(LatLng? pos) {
     setState(() {
       final CameraPosition cameraPosition =
           CameraPosition(target: pos!, zoom: 14);
@@ -114,44 +110,39 @@ class _AddressLocationScreenState extends State<AddressLocationScreen> {
     });
   }
 
-  Future<void> locationPermission() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  Future<void> _getLocationPermission() async {
+    final serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
       showToast(S.current.pleaseEnableYourLocation);
+      return;
     }
-
-    permission = await Geolocator.checkPermission();
+    LocationPermission permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
         showToast(S.current.locationPermissionsDenied);
       }
     }
-
     if (permission == LocationPermission.deniedForever) {
       showToast(S.current.locationPermissionsPermanentlyDenied);
     }
   }
 
-  Future<String> getAddressFromLatLong(LatLng position) async {
-    final List<Placemark> placemarks =
+  Future<String> _getAddressFromLatLong(LatLng position) async {
+    final placemarks =
         await placemarkFromCoordinates(position.latitude, position.longitude);
-    final Placemark place = placemarks[0];
+    final place = placemarks[0];
     return '${place.street}, ${place.subLocality}, ${place.locality}, ${place.postalCode}, ${place.country}';
   }
 
-  Future<LatLng?> locatePosition() async {
-    locationPermission();
-    final Position position = await Geolocator.getCurrentPosition(
+  Future<LatLng?> _locatePosition() async {
+    await _getLocationPermission();
+    final position = await Geolocator.getCurrentPosition(
       desiredAccuracy: LocationAccuracy.high,
     );
-    final LatLng latLng = LatLng(position.latitude, position.longitude);
+    final latLng = LatLng(position.latitude, position.longitude);
     _currentPosition = latLng;
-    final CameraPosition cameraPosition =
-        CameraPosition(target: latLng, zoom: 14);
+    final cameraPosition = CameraPosition(target: latLng, zoom: 14);
     _controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
     return _currentPosition;
   }
