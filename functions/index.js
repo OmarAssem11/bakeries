352874c9b11a40ed67d442d3a58ab92beec31ce8
Omar Assem <omarassem996@gmail.com>
2023-01-14 02:09:23 +0200

    create addToCart cloud function

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,65 @@ admin.initializeApp(functions.config().functions);
 
 const db = admin.firestore();
 
+exports.addToCart = functions.firestore
+  .document("users/{userId}/cart/{cartProductId}")
+  .onCreate(async (snapshot, context) => {
+    const cartCollection = db
+      .collection("users")
+      .doc(context.params.userId)
+      .collection("cart");
+    const cartQuerySnapshot = await cartCollection.get();
+    const cartDocs = cartQuerySnapshot.docs;
+    if (cartDocs.length === 0) return;
+    const cartProductsFromCartCollection = cartDocs.map((queryDocSnapshot) =>
+      queryDocSnapshot.data()
+    );
+    const productsCollection = db.collection("products");
+    const productDocSnapshot = await productsCollection
+      .doc(context.params.cartProductId)
+      .get();
+    const product = productDocSnapshot.data();
+    const cartProducts = await Promise.all(
+      cartProductsFromCartCollection.map(async (cartProduct) => {
+        const docSnapshot = await productsCollection
+          .doc(cartProduct.productModel.id)
+          .get();
+        return docSnapshot.data();
+      })
+    );
+    let productIsFromDifferentBakery = false;
+    let productAlreadyExistInCart = false;
+    for (const cartProduct of cartProducts) {
+      if (product.bakeryId !== cartProduct.bakeryId) {
+        productIsFromDifferentBakery = true;
+        break;
+      } else if (product.id === cartProduct.id) {
+        productAlreadyExistInCart = true;
+        break;
+      }
+    }
+    if (productIsFromDifferentBakery) {
+      return Promise.all(
+        cartQuerySnapshot.docs.map(async (cartDocSnapshot) => {
+          if (
+            cartDocSnapshot.data().productModel.id !==
+            productDocSnapshot.data().id
+          )
+            await cartDocSnapshot.ref.delete();
+        })
+      );
+    } else if (productAlreadyExistInCart) {
+      const oldCartProduct = cartProductsFromCartCollection.find(
+        (cartProduct) => {
+          return cartProduct.productModel.id === product.id;
+        }
+      );
+      return cartCollection.doc(product.id).update({
+        quantity: oldCartProduct.quantity + snapshot.data().quantity,
+      });
+    }
+  });
+
 exports.acceptOrder = functions.firestore
   .document("users/{userId}/orders/{orderId}")
   .onCreate(async (snapshot, context) => {
